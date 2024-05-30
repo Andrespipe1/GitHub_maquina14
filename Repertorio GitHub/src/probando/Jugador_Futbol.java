83621877a3b4d4166e4e62f2d308480c33ed4889
POO <andresftma@gmail.com>
2024-05-30 07:59:51 -0500

    a

---
+++ Repertorio GitHub/src/probando/Jugador_Futbol.java
@@ -0,0 +1,39 @@
+package probando;
+
+public class Jugador_Futbol {
+    public int cedula;
+    public String nombre;
+    public int camiseta;
+
+    public Jugador_Futbol(int cedula, String nombre, int camiseta) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.camiseta = camiseta;
+    }
+
+    public void mostrar_info(){
+        System.out.println("--- Jugador ---\nCedula: "+cedula+"--Nombre: "+nombre+"--Camiseta: "+camiseta);
+    }
+}
+
+class Defensa extends Jugador_Futbol {
+    public String posicion;
+    public int tarjetas;
+    public int goles;
+    public int faltas;
+
+    public Defensa(int cedula, String nombre,int camiseta,String posicion, int tarjetas, int goles, int faltas){
+        super(cedula, nombre, camiseta);
+        this.posicion=posicion;
+        this.tarjetas=tarjetas;
+        this.goles=goles;
+        this.faltas=faltas;
+    }
+    public void mostrar_info_Defensa(){
+        mostrar_info();
+        System.out.println("Posición: " + posicion);
+        System.out.println("Tarjetas: " + tarjetas);
+        System.out.println("Goles: " + goles);
+        System.out.println("Faltas: " + faltas);
+    }
+}
---
+++ Repertorio GitHub/src/probando/Main.java
@@ -0,0 +1,8 @@
+package probando;
+
+public class Main {
+    public static void main (String[] args){
+        Defensa jugador1 = new Defensa(1754270864,"Moises Caicedo",10,"MCD",12,8,6);
+        jugador1.mostrar_info_Defensa();
+    }
+}
